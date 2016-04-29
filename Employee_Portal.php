138961826633a8b56c6fd404d4629b9d6aca83e5
thridda <brian@thridda.com>
2016-04-29 14:24:34 -0400

    Abstracting Head, Menu, and Header of site.

--- Employee_Portal.php
+++ Employee_Portal.php
@@ -2,18 +2,11 @@
 session_start();
 ?>
 <html>
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
+<?php include 'Page_Head.php';?>
 <body>
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
+	
+<?php include 'Site_Header.php'; ?>
+
 <?php
 
 if( $_POST['EmpEmail'] > ""){
@@ -60,12 +53,8 @@ if( $_POST['EmpEmail'] > ""){
 if(  isset($_SESSION['validemp'])  ){
 	// valid user
 ?>
-<h2>Employee System Menu</h2>
-<ul>
-  <li><a href="SampleForm[CHANGE].php">Check Submissions</a></li>
-  <li><a href="Employee_Info.php">View or Update your information</a><br><br>&nbsp;</li>
-  <li><a href="Logout.php">Log out of the system</a></li>
-</ul>
+<?php include 'Site_Menu.php';?>
+
 <?php
 } else {
 	// not logged in yet
