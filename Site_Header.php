170fbcd4e89caf21c8fc397b288188dd2d762039
thridda <brian@thridda.com>
2016-05-02 16:18:43 -0400

    Added logo and renamed some links to be more sensible/modern

--- Site_Header.php
+++ Site_Header.php
@@ -4,8 +4,8 @@
       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
        <li><a href="SampleForm[CHANGE].php">Check Submissions</a></li>
-       <li><a href="Employee_Info.php">View or Update your information</a></li>
-       <li><a href="Logout.php">Log out of the system</a></li>
+       <li><a href="Employee_Info.php"><i class="material-icons dp48">User Account</i></a></li>
+       <li><a href="Logout.php">Log Out</a></li>
       </ul>
     </div>
   </nav>
