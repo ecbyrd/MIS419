d0f4501a56157862de49d97e9abf96c4f617b3ce
thridda <brian@thridda.com>
2016-05-02 12:04:36 -0400

    Removed site menu file as we moved into header for materialize integration

--- Site_Menu.php
+++
@@ -1,5 +0,0 @@
-<ul class="nav">
-  <li><a href="SampleForm[CHANGE].php">Check Submissions</a></li>
-  <li><a href="Employee_Info.php">View or Update your information</a><br><br>&nbsp;</li>
-  <li><a href="Logout.php">Log out of the system</a></li>
-</ul>
